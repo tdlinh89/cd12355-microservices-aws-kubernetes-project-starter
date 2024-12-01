a678f0cc158480395e485d8956f84c1c118021e3
LinhTD3 <linhtd3@fpt.com>
2024-12-01 10:39:36 +0700

    Commit analytics

--- analytics/app.py
+++ analytics/app.py
@@ -1,17 +1,47 @@
-import logging
 import os
-
+import logging
+import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime, timedelta
-from flask import jsonify
-from sqlalchemy import and_, text
-from random import randint
-
-from config import app, db
+from sqlalchemy import text
+from flask import jsonify, Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import db_username, db_password, db_host, db_port, db_name
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 
 
 port_number = int(os.environ.get("APP_PORT", 5153))
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+app.logger.setLevel(logging.DEBUG)
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    joined_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.id
+
+class Token(db.Model):
+    __tablename__ = "tokens"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    token = Column(String(6), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    used_at = Column(DateTime)
+
+    def __repr__(self):
+        return "<Token %r>" % self.id
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/health_check")
 def health_check():
@@ -21,7 +51,7 @@ def health_check():
 @app.route("/readiness_check")
 def readiness_check():
     try:
-        count = db.session.execute(text("SELECT COUNT(*) FROM tokens")).scalar()
+        count = db.session.query(Token).count()
     except Exception as e:
         app.logger.error(e)
         return "failed", 500
